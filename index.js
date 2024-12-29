56183cdb79fc7eef4f64e4a0b731b27df533dad6
Armando <armando@Armandos-MacBook-Pro.local>
2024-12-29 01:48:06 +0100

    GET query params (parameters)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require("@faker-js/faker");
+
 const app = express();
 const port = 3000;
 
@@ -10,24 +12,24 @@ app.get('/new-route', (req, res) => {
   res.send('Hi, I am a new route');
 })
 
-app.get('/products', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price: 1000
-  },
-  {
-    name: 'Product 2',
-    price: 2000
-  });
-})
-
 app.get('/products', (req, res) => { // send json
-  res.json({
-    name: 'Product 1',
-    price: 1000
-  });
-})
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit ; index++) {
+    products.push({
+      name: faker.commerce.productName(), //faker provides random productName
+      price: parseInt(faker.commerce.price(), 10), //parse String to Int
+      image: faker.image.url(), //provides random image Url
+    });
+  }
+  res.json(products);
+});
 
+//avoid misinterpretation of filter as ":id" placing filter first
+app.get('/products/filter', (req, res) => {
+  res.send('I am a filter'));
+})
 // use GET variables in url as ":id" with "req.params"
 app.get('/products/:id', (req, res) => {
   const {id} = req.params;
@@ -44,6 +46,18 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
     categoryId,
     productId,
   })
+})
+
+app.get('/users', (req,res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset ) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('There are no params')
+  }
 })
 
 app.listen(port, () => {
