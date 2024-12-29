0ad8cde2bbfd161fa81935910ee25545f4c6dc50
Armando <armando@Armandos-MacBook-Pro.local>
2024-12-29 16:21:50 +0100

    Async await, Promises and error catch

--- routes/products.router.js
+++ routes/products.router.js
@@ -6,8 +6,8 @@ const ProductService = require('./../services/product.service');
 const router = express.Router();
 const service = new ProductService();
 
-router.get('/', (req, res) => { // send json
-  const products = service.find();
+router.get('/', async (req, res) => { // send json
+  const products = await service.find();
   res.json(products);
 });
 
@@ -16,32 +16,38 @@ router.get('/filter', (req, res) => {
   res.send('I am a filter');
 })
 // use GET variables in url as ":id" with "req.params"
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const {id} = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 })
 
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
     res.status(201).json({
       message: 'created',
       data: body
     })
 })
 
-router.patch('/:id', (req,res) => {
-  const { id } = req.params
-  const body = req.body;
-  const product = service.update(id,body)
+router.patch('/:id', async (req,res) => {
+  try {
+    const { id } = req.params
+    const body = req.body;
+    const product = await service.update(id,body)
     res.json(product)
+  } catch(error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
 })
 
-router.delete('/:id', (req,res) => {
+router.delete('/:id', async (req,res) => {
   const { id } = req.params
   const body = req.body;
-  const rta = service.delete(id)
+  const rta = await service.delete(id)
     res.json(rta)
 })
 
