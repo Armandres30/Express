d111cc1777606f400d9060590adff15afcda48dd
Armando <armando@Armandos-MacBook-Pro.local>
2024-12-30 00:52:39 +0100

    Fix whitelist origin iand Heroku set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler');
 
@@ -7,6 +8,17 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed'));
+    }
+  }
+}
 app.use(cors());
 
 app.get('/', (req, res) => {
