7c36ac625fc3f8d8aefeb23751e2e6904f449cb0
Armando <armando@Armandos-MacBook-Pro.local>
2024-12-29 17:01:28 +0100

    Middleware for HttpErrors

--- routes/products.router.js
+++ routes/products.router.js
@@ -16,10 +16,14 @@ router.get('/filter', (req, res) => {
   res.send('I am a filter');
 })
 // use GET variables in url as ":id" with "req.params"
-router.get('/:id', async (req, res) => {
-  const {id} = req.params;
+router.get('/:id', async (req, res, next) => {
+  try{
+    const {id} = req.params;
   const product = await service.findOne(id);
   res.json(product);
+  } catch (error){
+    next(error);
+  }
 })
 
 router.post('/', async (req,res) => {
