41002db1dd916df48e21ae3bb7b2de6aace53ad6
Armando <armando@Armandos-MacBook-Pro.local>
2024-12-29 03:59:50 +0100

    CRUD POST, PUT, DELETE and Status codes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
+const express = require('express');
 
 function routerApi(app) {
-  app.use('/products', productsRouter); //define first part of url as /products/... extended by productsRouter
-  app.use('/categories', categoriesRouter);
-  app.use('/users', usersRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter); //define first part of url as /products/... extended by productsRouter
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
--- routes/products.router.js
+++ routes/products.router.js
@@ -24,11 +24,44 @@ router.get('/filter', (req, res) => {
 // use GET variables in url as ":id" with "req.params"
 router.get('/:id', (req, res) => {
   const {id} = req.params;
-  res.json({
-	  id,
-    name: 'Product 2',
-    price: 2000
-  });
+  if (id === 999){
+    res.status(404).json({
+      message: 'not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000
+    });
+  }
+})
+
+router.post('/', (req,res) => {
+  const body = req.body;
+    res.status(201).json({
+      message: 'created',
+      data: body
+    })
+})
+
+router.patch('/:id', (req,res) => {
+  const { id } = req.params
+  const body = req.body;
+    res.json({
+      message: 'created',
+      data: body,
+      id
+    })
+})
+
+router.delete('/:id', (req,res) => {
+  const { id } = req.params
+  const body = req.body;
+    res.json({
+      message: 'deleted',
+      id,
+    })
 })
 
 module.exports = router;
